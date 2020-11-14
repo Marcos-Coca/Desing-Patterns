f982ae8d14d5324161be6b227c1d4d611c68e434
Marcos Mercado <marcoscoca0905@gmail.com>
2020-11-14 19:38:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desing_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // var instance = new Singleton(); Error 'cause the constructor is private 
+            Console.WriteLine(Singleton.Instance.message);
+            Console.WriteLine(Singleton.Instance.message);
+        }
+
+    }
+}
+
---
+++ Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desing_Patterns
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string message = "";
+
+        private Singleton() {
+            this.message = "hola mundo";
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+    }
+}
