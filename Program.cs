341965de57c5d6a2b7dd485019a0706305def565
Marcos Mercado <marcoscoca0905@gmail.com>
2020-11-15 00:05:42 -0400

    Prototype Deep done but not complete

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace Desing_Patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // var instance = new Singleton(); Error 'cause the constructor is private 
-            Console.WriteLine(Singleton.Instance.message);
-            Console.WriteLine(Singleton.Instance.message);
+            Animal animal = new Animal() {Name="Dolly",Legs=4};
+            animal.Features = new Details() {Color="White",Breed="Sheep" };
+
+            Animal clonedAnimal = animal.Clone() as Animal;
+            clonedAnimal.Features = new Details() { Color = "Black", Breed = "Black Sheep" };
+
+            Console.WriteLine("Original Animal"+animal.Features.Color);
+            Console.WriteLine("Cloned Animal"+animal.Features.Color);
+            Console.WriteLine("Original Animal"+animal.Features.Breed);
+            Console.WriteLine("Cloned Animal"+animal.Features.Breed);
         }
 
     }
---
+++ Prototype.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desing_Patterns
+{
+    public class Animal: ICloneable
+    {
+       public int Legs { get; set; }
+       public string Name { get; set; }
+        public Details Features { get; set;}
+
+        public object Clone()
+        {
+            Animal clon = this.MemberwiseClone() as Animal;
+            Details deatails = new Details();
+            deatails.Color = this.Features.Color;
+            deatails.Color = this.Features.Breed;
+            clon.Features = deatails;
+            return clon;
+        }
+    }
+
+    public class Details
+    {
+        public string Color { get; set; }
+        public string Breed { get; set; }
+    }
+}
