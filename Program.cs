01534f7877ec8215b3b1ff539d5970f41238da15
Marcos Mercado <marcoscoca0905@gmail.com>
2020-11-15 12:09:14 -0400

    Default Program

--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ namespace Desing_Patterns
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // var instance = new Singleton(); Error 'cause the constructor is private 
-            Console.WriteLine(Singleton.Instance.message);
-            Console.WriteLine(Singleton.Instance.message);
         }
 
     }
